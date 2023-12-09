68043850b1b7e4676733f0a7acfc62a1b2bceb8c
Snigdhajyoti Ghosh <snigdhasjg@users.noreply.github.com>
2023-12-09 23:48:32 +0530

    Complete day 9

--- advent-of-code-2023/day9.py
+++ advent-of-code-2023/day9.py
@@ -1,30 +1,52 @@
 import logging
 
 LOG = logging.getLogger(__name__)
-LOG_LEVEL = logging.DEBUG
+LOG_LEVEL = logging.CRITICAL
 DAY = 9
-DEVELOPMENT_PHASE = True
+DEVELOPMENT_PHASE = False
 PART_1_ENABLE = True
-PART_2_ENABLE = False
+PART_2_ENABLE = True
 
 
 def part1_example():
     return """\
-        multi
-        line
-        input"""
+        0 3 6 9 12 15
+        1 3 6 10 15 21
+        10 13 16 21 30 45"""
+
+
+def all_difference(sequence: list[int]):
+    next_number = sequence[-1]
+    prev_number = sequence[0]
+
+    current_seq = sequence
+    sign = -1
+    while True:
+        next_seq = []
+        for idx in range(len(current_seq) - 1):
+            next_seq.append(current_seq[idx + 1] - current_seq[idx])
+        next_number += next_seq[-1]
+        prev_number += (sign * next_seq[0])
+        current_seq = next_seq
+        sign *= -1
+
+        if all(v == 0 for v in next_seq):
+            break
+    LOG.debug(f'For sequence {sequence}: previous number: {prev_number} & next number {next_number}')
+    return prev_number, next_number
 
 
 def part1(raw_input: str):
-    pass
+    sequences = [list(map(int, each_line.split(' '))) for each_line in raw_input.splitlines()]
+
+    return sum(map(lambda x: x[1], map(all_difference, sequences)))
 
 
 def part2_example():
-    return """\
-        multi
-        line
-        input"""
+    return part1_example()
 
 
 def part2(raw_input: str):
-    pass
+    sequences = [list(map(int, each_line.split(' '))) for each_line in raw_input.splitlines()]
+
+    return sum(map(lambda x: x[0], map(all_difference, sequences)))
--- advent-of-code-2023/run.py
+++ advent-of-code-2023/run.py
@@ -2,7 +2,7 @@ import logging
 import textwrap
 import os
 
-from day8 import (
+from day9 import (
     LOG_LEVEL,
     DAY,
     DEVELOPMENT_PHASE,
